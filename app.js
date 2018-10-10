151a87af26131a2878d5214cf93bdb9ae887e189
Le Nghia <nghia.neal@gmail.com>
2018-10-10 12:26:36 +0300

    initial

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const graphqlHTTP = require("express-graphql");
+
+const app = express();
+
+// Body Parser Middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, "../")));
+
+app.use("/graphql", graphqlHTTP({}));
+
+// DB config
+// const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err));
+
+// Use Routes
+// app.use();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Now listening for request on port ${port}"`);
+});
